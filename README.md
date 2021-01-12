2dfb8e083a1170ceda347b5cc5d39db45aa90827
Pearson Radu <pradu96@gmail.com>
2021-01-12 20:19:06 +0000

    added external property file import that breaks tests when used with @ActiveProfiles("test")

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-24497
+
+Spring Boot project to demonstrate bug from https://github.com/spring-projects/spring-boot/issues/24497
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
